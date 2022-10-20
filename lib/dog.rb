841946dbb4e49bdd88f2d5b5a2ca2d933eb7b616
sharon <sharon.chepkemoi@student.moringaschool.com>
2022-10-20 22:26:36 +0300

    Done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,37 @@
 class Dog
+    attr_accessor:name
 
-end
+    attr_accessor:breed
+
+    def initialize (name="Max", breed="Cockapoo")
+        @name = name
+        @breed= breed
+    end
+   end
+   kanye = Dog.new
+
+   kanye.name=("Cocker")
+
+  
+
+
+
+
+
+
+    # setter method
+  #def name=(name)
+  #  @name = name
+  #end
+
+  # getter method
+  #def name
+  #  @name
+ # end
+     
+    
+#    end
+
+   
+    
+
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,34 @@
 class Person
+     # setter method
+     attr_accessor:name
 
-end
+     attr_accessor:job
+
+     def initialize (name="Cal", job="Engineeer")
+         @name = name
+         @job= job
+     end
+    end
+    kanye = Person.new
+    kanye.name=("Garfield")
+    kanye.job=("Engineer")
+
+
+
+
+
+
+  #def name=(name)
+
+   # @name = name
+
+ # end
+
+  # getter method
+ # def name
+  #  @name
+    
+ # end
+  #end
+
+     
